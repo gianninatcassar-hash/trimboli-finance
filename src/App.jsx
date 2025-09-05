1eb02ce3de1979b7bcf180db8596626f68b29292
gianninatcassar-hash <gianninatcassar@outlook.com>
2025-09-05 22:15:36 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -200,7 +200,7 @@ function App() {
               { src: "/logos/westpac.png", alt: "Westpac" },
               { src: "/logos/cba.png", alt: "Commonwealth Bank" },
               { src: "/logos/anz.png", alt: "ANZ" },
-              { src: "/logos/ing.png", alt: "ING" },
+              { src: "/logos/ingg.png", alt: "ING" },
               { src: "/logos/suncorp.png", alt: "Suncorp" },
               { src: "/logos/macquarie.png", alt: "Macquarie" },
               { src: "/logos/bom.png", alt: "Bank of Melbourne" },
